9dcf67b407706f5278fc00df08429892ab035858
Jacob Tyo <jaketyo@gmail.com>
2016-10-04 09:03:26 -0700

    Finished Payload array

--- logger.php
+++ logger.php
@@ -20,18 +20,25 @@ class LoggerPlugin extends Plugin {
 		$ticket_id = $ticket->getId();
 		$ticket_number = $ticket->getNumber();
 		$ticket_url = $ost->getConfig()->getUrl() . 'scp/tickets.php?number=' . $ticket_number;
-		$ticket_subject = $ticket->getSubject();
+//		$ticket_subject = $ticket->getSubject();
 		$ticket_name = $ticket->getName();
 		$ticket_email = $ticket->getEmail();
-		//$ticket_topic = $ticket->getTopic()->getName();
-		//$ticket_lastMessage = $ticket->getLastMessage();
+//		$ticket_topic = $ticket->getTopic()->getName();
+//		$ticket_lastMessage = $ticket->getLastMessage();
 
+		// Create the payload array
 		$data = "payload=" . json_encode(array(
-			"ticket_id"		=> "{$ticket_id}",
-			"ticket_num"	=> "{$ticket_number}",
-			"ticket_url"	=> "{$ticket_url}"
+			"ticket_id"     => "{$ticket_id}",
+            "ticket_num"    => "{$ticket_number}",
+            "ticket_url"    => "{$ticket_url}",
+            "ticket_author" => "{$ticket_name}",
+            "ticket_email"  => "{$ticket_email}",
+            "ticket_subject"=> "_SUBJECT_",
+            "ticket_topic"  => "_TOPIC_",
+            "ticket_msg"    => "_MESSAGE_"
 		));
 
+		// Prepare and Curl the data
 		$ch = curl_init($logger_url);
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
@@ -42,10 +49,7 @@ class LoggerPlugin extends Plugin {
 		$curl_error = curl_error($ch);
 		curl_close($ch);
 
-		// DEBUG - This line outputs the array $data to stderr
-		file_put_contents('php://stderr', print_r($data, TRUE));
-
-		//Catch Curl errors and post them in the log file
+		// Catch Curl errors and post them in the log file
 		if ($curl_errno > 0) {
 			error_log('Slack Curl Error ' . $curl_error);
 		}
