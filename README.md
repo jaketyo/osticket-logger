0833ab7c2e1febcf16e0e15bc8c27316b17a9a18
Jacob Tyo <jaketyo@gmail.com>
2016-10-04 09:13:42 -0700

    Minor changes

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # osticket-logger
 ## Remote-Logging plugin for osTicket
-osticket-logger is a simple plugin that uses CURL to XPUT ticket details to an endpoint that is configured to ingest an array.
+osticket-logger is a simple plugin that uses CURL to POST ticket details to an endpoint that is configured to ingest a JSON array.
 
 ### Known Bugs
 `$ticket->getTopic()->getName()` returns nil.
 `$ticket->getLastMessage()` returns nil.
+`$ticket->getSubject()` returns nil.
--- plugin.php
+++ plugin.php
@@ -2,10 +2,10 @@
 
 return array(
 	'id' =>				'osticket:logger',
-	'version' =>		'0.0.1',
+	'version' =>		'0.2',
 	'name' =>			'Logger Plugin',
 	'author' =>			'Jacob Tyo',
-	'description' =>	'Posts ticket details to a endpoint capible of accepting XPUT containing ticket info when a new ticket is created',
+	'description' =>	'Posts ticket details to a endpoint capible of accepting POST containing ticket info when a new ticket is created',
 	'url' =>			'https://github.com/jaketyo/osticket-logger',
 	'plugin' =>			'logger.php:LoggerPlugin',
 );
