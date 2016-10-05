3009de39bc923345b8f892a54f6023b765ced392
Jacob Tyo <jaketyo@gmail.com>
2016-10-05 11:53:20 -0700

    Implement first test

---
+++ Tests/LoggerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests;
+
+class LoggerPluginTest extends \PHPUnit_Framework_TestCase
+{
+	public function testTrueIsTrue()
+	{
+		$foo = true;
+		$this->assertTrue($foo);
+	}
+}
+
+?>
